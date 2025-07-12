cfd191baac37f38ccfb3071cc984a871aa22d39d
rioscris <mrioscristian@gmail.com>
2025-07-12 17:45:25 -0300

    basic view

---
+++ src/components/Clock.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Clock: React.FC = () => {
+    return (
+        <div>
+            <button>
+                00:00
+            </button>
+        </div>
+    );
+};
+
+export default Clock;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar: React.FC = () => {
+    return (
+        <nav>
+            <a href="#config">Configurations</a>
+        </nav>
+    );
+}
+
+export default Navbar;
