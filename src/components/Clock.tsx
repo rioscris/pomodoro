683746adcb32582ed4ef15b5c808dbcfb1d8f4be
rioscris <mrioscristian@gmail.com>
2025-07-12 17:56:51 -0300

    basic timer

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -1,10 +1,33 @@
 import React from 'react';
+import { useTimer } from 'react-timer-hook';
+
+const minutesFromNow = (minutes: number): Date => {
+    const date = new Date();
+    date.setMinutes(date.getMinutes() + minutes);
+    return date;
+}
+
+const timeToString = (number: number): string => {
+    return number < 10 ? `0${number}` : `${number}`;
+}
 
 const Clock: React.FC = () => {
+    const defaultExpiryTimestamp = minutesFromNow(25);
+    const onExpire = () => {
+        console.warn("Timer expired!");
+    }
+    const { minutes, seconds, pause, isRunning, resume } = useTimer({ expiryTimestamp: defaultExpiryTimestamp, onExpire, autoStart: false })
+    const toggleTimer = () => {
+        if (isRunning) {
+            pause();
+        } else {
+            resume();
+        }
+    }
     return (
         <div>
-            <button>
-                00:00
+            <button onClick={toggleTimer} style={{ fontSize: '2em', padding: '10px 20px' }}>
+                {timeToString(minutes)}:{timeToString(seconds)}
             </button>
         </div>
     );
